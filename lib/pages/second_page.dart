8cf28f5224cfa4928afc8fbf24a6f54ef7312942
mihir <mihir.gorasiya.ladila@gmail.com>
2023-01-05 09:11:33 +0530

    --

--- lib/pages/second_page.dart
+++ lib/pages/second_page.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:widget_to_photo/pages/home.dart';
 
 class SecondPage extends StatelessWidget {
